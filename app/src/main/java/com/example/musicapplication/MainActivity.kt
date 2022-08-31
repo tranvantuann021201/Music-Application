cab3623c6afa5d089f00d5457ae113297942e307
tranvantuann021201 <tvtuan0212@gmail.com>
2022-08-31 12:10:52 +0700

    man hinh xoay ngang

--- app/src/main/java/com/example/musicapplication/MainActivity.kt
+++ app/src/main/java/com/example/musicapplication/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide()
+        //supportActionBar?.hide()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navController = this.findNavController(R.id.myNavHostFragment)
